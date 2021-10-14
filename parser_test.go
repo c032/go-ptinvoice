1207ed2658bc3321f382dd9f0d6476ded03a4fb3
c032 <c032+github@wark.io>
2021-10-14 13:13:32 +0000

    `TestParseMessage` -> `TestParse`

--- parser_test.go
+++ parser_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/c032/go-ptinvoice"
 )
 
-func TestParseMessage_Invoice(t *testing.T) {
+func TestParse_Invoice(t *testing.T) {
 	const input = `A:123456789*B:999999990*C:PT*D:FT*E:N*F:20191231*G:FT AB2019/0035*H:CSDF7T5H-0035*I1:PT*I2:12000.00*I3:15000.00*I4:900.00*I5:50000.00*I6:6500.00*I7:80000.00*I8:18400.00*J1:PT-AC*J2:10000.00*J3:25000.56*J4:1000.02*J5:75000.00*J6:6750.00*J7:100000.00*J8:18000.00*K1:PT-MA*K2:5000.00*K3:12500.00*K4:625.00*K5:25000.00*K6:3000.00*K7:40000.00*K8:8800.00*L:100.00*M:25.00*N:64000.02*O:513600.58*P:100.00*Q:kLp0*R:9999*S:TB;PT00000000000000000000000;513500.58`
 
 	expectedFields := ptinvoice.Fields{
@@ -68,7 +68,7 @@ func TestParseMessage_Invoice(t *testing.T) {
 	}
 }
 
-func TestParseMessage_SimplifiedInvoice(t *testing.T) {
+func TestParse_SimplifiedInvoice(t *testing.T) {
 	const input = `A:123456789*B:999999990*C:PT*D:FS*E:N*F:20190812*G:FS CDVF/12345*H:CDF7T5HD-12345*I1:PT*I7:0.65*I8:0.15*N:0.15*O:0.80*Q:YhGV*R:9999*S:NU;0.80`
 
 	expectedFields := ptinvoice.Fields{
@@ -106,7 +106,7 @@ func TestParseMessage_SimplifiedInvoice(t *testing.T) {
 	}
 }
 
-func TestParseMessage_ProFormaInvoice(t *testing.T) {
+func TestParse_ProFormaInvoice(t *testing.T) {
 	const input = `A:500000000*B:123456789*C:PT*D:PF*E:N*F:20190123*G:PF G2019CB/145789*H:HB6FT7RV-145789*I1:PT*I2:12345.34*I3:12532.65*I4:751.96*I5:52789.00*I6:6862.57*I7:32425.69*I8:7457.91*N:15072.44*O:125165.12*Q:r/fY*R:9999`
 
 	expectedFields := ptinvoice.Fields{
@@ -148,7 +148,7 @@ func TestParseMessage_ProFormaInvoice(t *testing.T) {
 	}
 }
 
-func TestParseMessage_TransportDocument(t *testing.T) {
+func TestParse_TransportDocument(t *testing.T) {
 	const input = `A:500000000*B:123456789*C:PT*D:GT*E:N*F:20190720*G:GT G234CB/50987*H:GTVX4Y8B-50987*I1:0*N:0.00*O:0.00*Q:5uIg*R:9999`
 
 	expectedFields := ptinvoice.Fields{
@@ -183,7 +183,7 @@ func TestParseMessage_TransportDocument(t *testing.T) {
 	}
 }
 
-func TestParseMessage_InvoiceWithForeignTaxRate(t *testing.T) {
+func TestParse_InvoiceWithForeignTaxRate(t *testing.T) {
 	const input = `A:123456789*B:4443332215*C:FR*D:FT*E:N*F:20190526*G:ABC BNH/4561*H:DK5ZJ2HN-4561*I1:FR*I7:100.00*I8:20.00*N:20.00*O:120.00*Q:YJRE*R:9999`
 
 	expectedFields := ptinvoice.Fields{
@@ -220,7 +220,7 @@ func TestParseMessage_InvoiceWithForeignTaxRate(t *testing.T) {
 	}
 }
 
-func TestParseMessage_TaxCorrectionDebitNote(t *testing.T) {
+func TestParse_TaxCorrectionDebitNote(t *testing.T) {
 	const input = `A:123456789*B:500000000*C:PT*D:ND*E:N*F:20190216*G: M1F KLG/6145*H: RQD8L6DG-6145*I1:PT-MA*I6:26.50*N:26.50*O:26.50*Q:h1rB*R:9999`
 
 	expectedFields := ptinvoice.Fields{
@@ -256,7 +256,7 @@ func TestParseMessage_TaxCorrectionDebitNote(t *testing.T) {
 	}
 }
 
-func TestParseMessage_InvoiceIssuedUnderVATMarginScheme(t *testing.T) {
+func TestParse_InvoiceIssuedUnderVATMarginScheme(t *testing.T) {
 	const input = `A:500000000*B:123456789*C:PT*D:FT*E:N*F:20191124*G:NF 19A/789145*H: JL9DS4TT-789145*I1:PT-AC*I7:50.00*I8:9.00*L:1000.00*N:9.00*O:1059.00*Q:d8/K*R:9999`
 
 	expectedFields := ptinvoice.Fields{
